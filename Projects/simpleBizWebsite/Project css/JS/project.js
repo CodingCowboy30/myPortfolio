8cae6bcd40e16927d03d701e445aaaa0f70bdc94
Martez Carr <113547475+CodingCowboy30@users.noreply.github.com>
2023-10-02 16:31:05 -0500

    Portfolio and projects and dir added

---
+++ Projects/simpleBizWebsite/Project css/JS/project.js
@@ -0,0 +1,15 @@
+console.log("connected-Martez");
+
+ let myName = "Martez" 
+// this variable is myStr and its value is hello its typeof is string
+ let myNum = 71
+ let myBool = true
+ let b 
+ let myArr = [false, "anotherString", 24, ["a", b, 0], "stingagain"]
+ document.getElementsByTagName("article").item(0).innerHTML= "Game Headz"
+
+function myKidsName(){
+// console.log("my function is working")
+return "my loves"; 
+}
+// myKidsName()
